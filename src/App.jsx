aa6693db9fc4e85b4fe129531ee380eb3f1c7e79
Smaidziunas <sdamarcius@gmail.com>
2022-12-06 08:47:24 +0200

    router; page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import UserOnlyPage from './pages/UserOnlyPage';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>React</h1>
+    <div className='App container'>
+      <Switch>
+        {/* jeigu rasom keleta zodziu reikia taprelio negalima camel case */}
+        <Route path={'/user-page'}>
+          <UserOnlyPage />
+        </Route>
+        <Route path={'/login'}>
+          <LoginPage />
+        </Route>
+        <Route path={'/'} exact>
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
