6155804e005df414eac633bd3a72aaf9ace6a0c7
Smaidziunas <sdamarcius@gmail.com>
2022-12-06 13:08:59 +0200

    restoring form lecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,61 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import UserOnlyPage from './pages/UserOnlyPage';
 import Header from './components/Header';
-import LoginForm from './components/LoginForm';
+import { useState } from 'react';
 
 function App() {
+  const history = useHistory();
+  const [userEmail, setUserEmail] = useState('');
+  const isUserLoggedIn = !!userEmail;
+
+  //
+  const handleLogin = (newLoginObj) => {
+    // console.log('handleLogin in LoginPage', newLoginObj);
+    // if login success we redirect to userOnly page
+    // imituojam teisinga email
+    const validEmail = 'james@bond.com';
+    if (newLoginObj.email === validEmail) {
+      // login success
+      console.log('login success');
+      // redirect
+      setUserEmail(newLoginObj.email);
+      history.push('/user-page');
+    } else {
+      // login fails
+      console.log('login fails');
+    }
+  };
+
+  const handleLogout = () => {
+    setUserEmail('');
+    history.push('/login');
+  };
+
   return (
+    <AuthContext.Provider value {555}>
     <div className='App container'>
-      <Header />
-      {/* <LoginForm /> */}
+      <Header
+        onLogout={handleLogout}
+        userEmail={userEmail}
+        isUserLoggedIn={isUserLoggedIn}
+      />
       <Switch>
         <Route path={'/user-page'}>
-          <UserOnlyPage />
+          <UserOnlyPage onLogout={handleLogout} />
         </Route>
         <Route path={'/login'}>
-          <LoginPage />
+          <LoginPage onLogin={handleLogin} />
         </Route>
         <Route path={'/'} exact>
           <HomePage />
         </Route>
       </Switch>
     </div>
+    
+    </AuthContext.Provider>
   );
 }
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,14 @@
+import { useHistory } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
 
 function LoginPage(props) {
-  const handleLogin = (newLoginObj) => {
-    // atsaspausdinam loginObj is loginForm
-    console.log('handleLogin in LoginPage', newLoginObj);
-    // if login success we redirect to userOnly page
-    const validEmail = 'james@bond.com';
-    if (newLoginObj.email === validEmail) {
-      // login success
-      console.log('login success');
-      //redirect
-    } else {
-      console.log('nepavyko prisijungti');
-    }
-  };
+  // const history = useHistory();
 
   return (
     <div>
       <h1>LoginPage</h1>
       <p>Please login</p>
-      <LoginForm onLogin={handleLogin} />
+      <LoginForm onLogin={props.onLogin} />
     </div>
   );
 }
