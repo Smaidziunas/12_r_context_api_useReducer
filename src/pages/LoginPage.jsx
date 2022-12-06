d22fef1bb857b4189e1162bc76f706d514471af8
Smaidziunas <sdamarcius@gmail.com>
2022-12-06 09:50:27 +0200

    passing loginObj to parent LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,18 @@
 import LoginForm from '../components/LoginForm';
 
 function LoginPage(props) {
+  const handleLogin = (newLoginObj) => {
+    // atsaspausdinam loginObj is loginForm
+    console.log('handleLogin in LoginPage', newLoginObj);
+    // if login success we redirect to userOnly page
+    const validEmail = 'james@bond.com';
+  };
+
   return (
     <div>
       <h1>LoginPage</h1>
       <p>Please login</p>
-      <LoginForm />
+      <LoginForm onLogin={handleLogin} />
     </div>
   );
 }
