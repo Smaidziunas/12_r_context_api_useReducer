377840911e93e718ba3d97bb705dbd79cc41b4c2
Smaidziunas <sdamarcius@gmail.com>
2022-12-06 08:38:57 +0200

    init empty pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage(props) {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>Welcome guest</p>
+    </div>
+  );
+}
+export default HomePage;
---
+++ src/pages/UserOnlyPage.jsx
@@ -0,0 +1,9 @@
+function UserOnlyPage(props) {
+  return (
+    <div>
+      <h1>UserOnlyPage</h1>
+      <p>Welcome JAMES</p>
+    </div>
+  );
+}
+export default UserOnlyPage;
